31988385064ed49133ea551414ef0d036ccf3ff9
Mr-Strange-Danger <99674248+Mr-Strange-Danger@users.noreply.github.com>
2022-05-02 23:09:22 +0530

    update
    Co-Authored-By: NiyamTandel <104680473+NiyamTandel@users.noreply.github.com>

---
+++ 99_Personal_Space/99_Private_Project/CardGame.py
@@ -0,0 +1,6 @@
+#User using Visual Studio Code
+#User using Git feature from Visual Studio Code
+
+
+#project_of_Probability
+dmd_k,dmd_q,dmd_j,dmd_1,dmd_2,dmd_3,dmd_4,dmd_5,dmd_6,dmd_7,dmd_8,dmd_9,dmd_10="Diamond_King","Diamond_Queen","Diamond_","","","","","",""
---
+++ 99_Personal_Space/99_Private_Project/tetra.py
@@ -0,0 +1,7 @@
+#User using Visual Studio Code
+#User using Git feature from Visual Studio Code
+
+
+def h1e(i1):
+    print('hi ',str(input('Enter Your Name  =  ')),', I am Yt1')
+print(h1e(1))
